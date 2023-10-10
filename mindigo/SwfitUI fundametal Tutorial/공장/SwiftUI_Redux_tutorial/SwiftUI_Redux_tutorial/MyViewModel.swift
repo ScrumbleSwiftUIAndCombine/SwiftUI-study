de96409a394c4147b8c6cff8cf7e588ae27d0e02
mingging <minimani0421@gmail.com>
2023-10-11 05:30:04 +0900

    ADD: 7회차 강의 정리 내용 추가

---
+++ mindigo/SwfitUI fundametal Tutorial/공장/SwiftUI_Redux_tutorial/SwiftUI_Redux_tutorial/MyViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MyViewModel.swift
+//  SwiftUI_Redux_tutorial
+//
+//  Created by min on 2023/10/11.
+//
+
+import SwiftUI
+
+class MyViewModel: ObservableObject {
+    @Published var appTitle: String = "일상"
+}
